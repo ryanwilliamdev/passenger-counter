02a0b432e8065a7455e97576108219ee3668ab70
ryanwilliamdev <rywilldav@gmail.com>
2023-03-31 13:05:53 -0400

    fix increment function, was not incrementing to 1 on first click

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-    countEl.innerText = count++;
+    count = count + 1;
+    countEl.innerText = count;
 }
 
+function save() {
+    console.log(count);
+}
+
+save();

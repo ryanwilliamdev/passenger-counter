ad4c2f9d30f4ca6066bb848511510adddcdfb38e
ryanwilliamdev <rywilldav@gmail.com>
2023-03-31 10:39:15 -0400

    initial commit. add index.html, index.js and style.css

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.getElementById("count").innerText = 5
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
+// console.log(count);
+
+let myAge = 34;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);

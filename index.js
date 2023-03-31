175be20a4fa07790d235cba74ab6b2eb4475afba
ryanwilliamdev <rywilldav@gmail.com>
2023-03-31 12:48:36 -0400

    small refactor to increment function, add countEl variable

--- index.js
+++ index.js
@@ -7,10 +7,14 @@
 
 
 // initialize the count as 0
-let count = 0;
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
-function increment() {
-    document.getElementById("count").innerText = count++;
-}
 // change the count-el in index.html
+
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    countEl.innerText = count++;
+}
+
